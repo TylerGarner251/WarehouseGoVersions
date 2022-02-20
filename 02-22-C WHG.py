60ed430ea10c54ce56dcc296f1bce5c14c9e2db8
WraithJimbob <95284445+WraithJimbob@users.noreply.github.com>
2022-02-20 12:44:10 +0000

    Class Function and refresh creation
    Created class function, refresh function and fixed the deleted button, which not deletes the item and refresh the listbox to match what is in the folders

---
+++ 02-22-C WHG.py
@@ -0,0 +1,99 @@
+import unittest
+import tkinter as tk
+from tkinter import *
+import sys
+import os
+import time
+
+root = Tk()
+root.title('WarehouseGo')
+class WarehouseGO(tk.Frame):
+    def __init__(self,*args,**kwargs):
+        tk.Frame.__init__(self,*args,**kwargs)
+        def refresh(self):
+            self.destroy()
+            self.__init__()
+        
+        ##__Background options__##
+        defaultBackground = "#2D2D2D"
+        LightMode = "light grey"
+
+        ##__creating window as root__##
+        # creates title,
+        # size of the window, 
+        # stops window resize, 
+        # colour of background
+        root.geometry("920x480")
+        root.resizable(width=0, height=0)
+        root.configure(bg=defaultBackground)
+
+        ##__Displaying on the window__##
+        # title label,
+        # colour of the background label
+        # lable text font and colour
+        # packs label to work
+        displayedMessage = tk.Label(text="WarehouseGo",font=("Courier", 20, "bold"),fg = ("white"),bg=defaultBackground)
+        displayedMessage.grid(row=0, column=0)
+
+        ##__Creating entry box to display__##
+        # Entry box font and text
+        productEntry = Entry(root,font=("Courier", 12), border = 3)
+        productEntry.grid(row=1, column=0, padx = 5, pady = 5)
+
+        ##__Grabbing Product Files__##
+        # gets directory
+        path = 'C:\\Users\\tyler\\Downloads\\resources'
+        productFile = os.listdir(path)
+
+        ##__Creating List box to display__##
+        # creates shape and visual of the list box
+        listbox = tk.Listbox(root,height = 10, width = 30, border = 3)
+        listbox.grid(row=3, column=0, pady = 10)
+
+        ##__fillouts the listbox__##
+        def fillout(event):
+            productEntry.delete(0, END)
+            productEntry.insert(0, listbox.get(ANCHOR))
+
+        ##__narrows down the listbox from entry search__##
+        # grabs the entrybox
+        # puts characters in list and compares to listbox
+        def scankey(event):
+            val = event.widget.get()
+    
+            if val == "":
+                data = productFile
+            else:
+                data = []
+                for item in productFile:
+                    if val.upper() in item.upper():
+                        data.append(item)
+            update(data)
+
+        def update(data):
+            listbox.delete(0, "end")
+            for item in data:
+                listbox.insert("end", item)
+
+
+        # Delete Function
+        def delete():
+            deleteEntry = productEntry.get()
+            str(deleteEntry)
+            listbox.delete(ANCHOR)
+            os.remove(path + "\\" + deleteEntry)
+            refresh(warehouseGO)
+
+
+        # Delete button
+        deleteButton =  Button(root, text="Delete", command=delete)
+        deleteButton.grid(row=2, column=2)
+
+        #__listbox bind__#
+        listbox.bind("<<ListboxSelect>>", fillout)
+        #__Entry search bind__#
+        productEntry.bind("<KeyRelease>", scankey)
+
+        update(productFile)  # Add the items in to the listbox
+warehouseGO = WarehouseGO()
+root.mainloop()
